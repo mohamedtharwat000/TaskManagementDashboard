364856157f5020b2cabc0d50770f397e125fef82
mo <mo@criticalfuture.co.uk>
2024-08-07 11:09:49 +0300

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+// import TaskList from './components/TaskList';
+// import TaskForm from './components/TaskForm';
+// import FilterButtons from './components/FilterButtons';
+// import SearchBar from './components/SearchBar';
+// import SortOptions from './components/SortOptions';
+
+const App: React.FC = () => {
+  return (
+    <Container>
+      <h1>Task Management Dashboard q</h1>
+      {/* <TaskForm />
+      <FilterButtons />
+      <SearchBar />
+      <SortOptions />
+      <TaskList /> */}
+    </Container>
+  );
+};
+
+export default App;
