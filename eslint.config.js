0f7a187cbfb31b3e9e27bc9efe3268a70081dde8
mo <mo@criticalfuture.co.uk>
2024-08-07 11:07:24 +0300

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import eslint from '@eslint/js';
+import tslint from 'typescript-eslint';
+import reactLint from 'eslint-plugin-react';
+import jsxLint from 'eslint-plugin-jsx-a11y';
+// import stylistic from '@stylistic/eslint-plugin';
+import reactHooksLint from 'eslint-plugin-react-hooks';
+import prettierPluginLint from 'eslint-plugin-prettier';
+import prettierConfigLint from 'eslint-config-prettier';
+
+export default tslint.config(
+  eslint.configs.recommended,
+  ...tslint.configs.recommended,
+  jsxLint.flatConfigs.recommended,
+  {
+    ...reactLint.configs.flat.recommended,
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    name: 'reactHooksLint',
+    plugins: { 'react-hooks': reactHooksLint },
+    rules: {
+      ...reactHooksLint.configs.recommended.rules,
+    },
+  },
+  {
+    name: 'prettierLint',
+    plugins: { prettier: prettierPluginLint },
+    rules: { ...prettierConfigLint.rules },
+  },
+  // stylistic.configs['recommended-flat'],
+  // stylistic.configs['disable-legacy'],
+);
