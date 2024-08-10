f2ccc34cee1115f89d571ab3387dc56aa79e738d
mo <mo@criticalfuture.co.uk>
2024-08-10 11:21:33 +0300

    add global types

---
+++ src/types/Globals.ts
@@ -0,0 +1,17 @@
+export interface SortOptions {
+  by: 'noSort' | 'creationDate' | 'dueDate';
+  direction: 'asc' | 'desc';
+}
+
+export interface FilterOptions {
+  status: 'all' | 'completed' | 'incomplete';
+  priority: 'normal' | 'high' | 'low';
+}
+
+export default interface Globals {
+  theme: 'light' | 'dark';
+  error: string;
+  success: string;
+  sort: SortOptions;
+  filter: FilterOptions;
+}
--- src/types/Task.ts
+++ src/types/Task.ts
@@ -1,23 +1,21 @@
-import { nanoid } from 'nanoid';
-
 export default class Task {
   public id: string;
-  public creationDate: string;
   public title: string;
   public description: string;
+  public creationDate: string;
   public dueDate: string;
-  public priority: number;
+  public priority: 'normal' | 'high' | 'low';
   public completed: boolean;
   public tags: string[];
 
   constructor(taskProperties: Partial<Task>) {
-    this.id = nanoid();
-    this.creationDate = Date.now().toString();
-    this.title = taskProperties.title || '';
-    this.description = taskProperties.description || '';
-    this.dueDate = taskProperties.dueDate || '';
-    this.priority = taskProperties.priority || 0;
-    this.completed = taskProperties.completed || false;
-    this.tags = taskProperties.tags || [];
+    this.id = Math.random().toString().slice(2, 4);
+    this.creationDate = new Date().getDate().toString();
+    this.title = taskProperties.title ?? '';
+    this.description = taskProperties.description ?? '';
+    this.dueDate = taskProperties.dueDate ?? '';
+    this.priority = taskProperties.priority ?? 'normal';
+    this.completed = taskProperties.completed ?? false;
+    this.tags = taskProperties.tags ?? [];
   }
 }
