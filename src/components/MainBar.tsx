7cc84ca58dfc8cd6e21d2a59c8005c8ff9ae12d7
mo <mo@criticalfuture.co.uk>
2024-08-10 11:24:15 +0300

    Create MainBar.tsx

---
+++ src/components/MainBar.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { Container, Row, Col, Button, Modal } from 'react-bootstrap';
+import SearchBar from './SearchBar';
+import FilterOptions from './FilterOptions';
+import SortOptions from './SortOptions';
+import TaskForm from './TaskForm';
+
+const MainBar = () => {
+  const [taskFormShow, setTaskFormShow] = useState(false);
+
+  const toggleTaskForm = () => setTaskFormShow(!taskFormShow);
+
+  return (
+    <Container className="my-2">
+      <Row className="justify-content-center justify-content-sm-between align-items-center g-4">
+        <Col
+          className="fs-1 text-center text-lg-start"
+          sm={12}
+          lg={4}
+          xl={2}
+        >
+          Tasks
+        </Col>
+        <Col
+          sm={12}
+          lg={8}
+          xl={4}
+        >
+          <SearchBar />
+        </Col>
+        <Col
+          sm={12}
+          lg={12}
+          xl={4}
+          className="d-flex justify-content-center align-items-center flex-nowrap"
+        >
+          <SortOptions />
+          <FilterOptions />
+        </Col>
+
+        <Col
+          sm={12}
+          lg={12}
+          xl={2}
+          className="d-flex justify-content-center justify-content-xl-end"
+        >
+          <Button
+            variant="primary"
+            onClick={() => {
+              toggleTaskForm();
+            }}
+          >
+            Create Task
+          </Button>
+        </Col>
+      </Row>
+
+      <Modal
+        show={taskFormShow}
+        onHide={() => {
+          toggleTaskForm();
+        }}
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Create Task</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <TaskForm toggleTaskForm={toggleTaskForm} />
+        </Modal.Body>
+      </Modal>
+    </Container>
+  );
+};
+
+export default MainBar;
