3f1ea600ef9bda0b83c8dffac54bd475eda04922
mo <mo@criticalfuture.co.uk>
2024-08-10 11:24:11 +0300

    Create Main.tsx

---
+++ src/components/Main.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import MainBar from './MainBar';
+import TaskList from './TaskList';
+import NotificationAlert from './Alert';
+
+const Main = () => {
+  return (
+    <div style={{ minWidth: '400px' }}>
+      <NotificationAlert />
+      <MainBar />
+      <TaskList />
+    </div>
+  );
+};
+
+export default Main;
