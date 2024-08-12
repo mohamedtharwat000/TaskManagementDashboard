b3ffb418d4ac005aed74805749eeea1aa44ae5ea
mo <mo@criticalfuture.co.uk>
2024-08-12 06:24:22 +0300

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { memo } from 'react';
 import MainBar from './MainBar';
 import TaskList from './TaskList';
 import NotificationAlert from './Alert';
 
 const Main: React.FC = () => {
   return (
-    <div style={{ minWidth: '400px' }}>
+    <>
       <NotificationAlert />
       <MainBar />
       <TaskList />
-    </div>
+    </>
   );
 };
 
-export default Main;
+export default memo(Main);
